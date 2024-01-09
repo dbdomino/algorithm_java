659a14e217ef47f3748741b9533fa3ed9aaac4ca
dominod <dbdomino@naver.com>
2024-01-09 12:40:18 +0900

    프로그래머스 이진반복

--- algorithm_java/src/main/java/com/ex/algorithm_java/ex01/App01.java
+++ algorithm_java/src/main/java/com/ex/algorithm_java/ex01/App01.java
@@ -1,9 +1,29 @@
 package com.ex.algorithm_java.ex01;
-
+// 프로그래머스 이진반복
 public class App01 {
+	private int countZeros(String s) { // 문자열의 0의 개수 세기
+		int zeros = 0;
+		for (char c : s.toCharArray()) {
+			if (c == '0' ) zeros++;
+		}
+		return zeros;
+	}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public int[] solution(String s) {
+		int loop = 0;
+		int removeCount = 0;
+
+		while (!s.equals("1")) {
+			int zeros = countZeros(s); //0의 개수
+			loop += 1;
+			removeCount += zeros;
+
+			int ones = s.length() - zeros;
+//			s = Integer.toBinaryString(ones);// 같은소리
+			s = Integer.toString(ones,2);
+
+		}
+		return new int[] {loop, removeCount};
 
 	}
 
