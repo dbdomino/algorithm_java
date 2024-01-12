940e26fdb40f0d1fc472eb76c6ce9e5d5004bc23
dominod <76812646+dbdomino@users.noreply.github.com>
2024-01-12 17:04:50 +0900

    [Bronze V] Title: A+B - 4, Time: 124 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -1,9 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         //이 문제에서 가장 중요한 점이 파일 종료 조건이 없이 그냥 입력이 주어졌다는 것이다.
         //즉 입력에서 더이상의 읽을 수 있는 데이터가 존재하지 않을 때 반복문을 종료하라는 것이다.
+        /*
         Scanner in=new Scanner(System.in);
         while(in.hasNextInt()){
             int a=in.nextInt();
@@ -11,5 +16,26 @@ public class Main {
             System.out.println(a+b);
         }
         in.close();
+         */
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        String str;
+        try {
+            while ((str = br.readLine()) != null) {
+
+                st = new StringTokenizer(str, " ");
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+                sb.append(a + b).append("\n");
+
+            }
+        }catch (Exception e) {
+
+        } finally {
+            System.out.print(sb);
+        }
+
     }
 }
