5554ebb3782311281d347173061d505a996cf6fb
dominod <76812646+dbdomino@users.noreply.github.com>
2024-01-23 16:16:01 +0900

    [Bronze IV] Title: 숫자의 합, Time: 132 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String str = br.readLine();
+
+        int sum=0;
+        for(int i = 0; i<n;i++)
+//            sum += Integer.parseInt(String.valueOf(str.charAt(i)));
+            sum += str.charAt(i) -'0'; // CHAR - '0'  문자를 숫자로 변환하기
+
+        System.out.println(sum);
+        br.close();
+    }
+}
