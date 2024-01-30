a9ad7a16b8c3d8464261881f1dc9365ccf706f61
dominod <dbdomino@naver.com>
2024-01-30 12:25:40 +0900

    백준 심화

---
+++ algorithm_java/src/main/java/com/ex/baekjoon/심화/ex25083.java
@@ -0,0 +1,14 @@
+package com.ex.baekjoon.심화;
+
+public class ex25083 {
+    public static void main(String[] args) {
+        //
+        String s = "         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |";
+        System.out.print(s);
+    }
+}
