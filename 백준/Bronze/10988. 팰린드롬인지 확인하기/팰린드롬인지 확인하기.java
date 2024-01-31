deb0ac3652f7b30eb49f275afceb79bbc027d1ee
dominod <76812646+dbdomino@users.noreply.github.com>
2024-01-31 22:09:52 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 120 ms, Memory: 14256 KB -BaekjoonHub

---
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String a = br.readLine();
+        int n1 = 0;
+        int n2 = a.length()-1;
+        int result = 1;
+        while (n1 < n2) {
+            if (a.charAt(n1) != a.charAt(n2)) { result = 0; break; }
+            n1++; n2--;
+        }
+        System.out.print(result);
+
+    }
+}
