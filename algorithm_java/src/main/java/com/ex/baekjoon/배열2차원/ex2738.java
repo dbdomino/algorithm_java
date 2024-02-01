473d679979508f040d7114a4fcfa5238ec4e5619
dominod <dbdomino@naver.com>
2024-02-01 22:28:02 +0900

    2차원 배열

--- algorithm_java/src/main/java/com/ex/baekjoon/배열2차원/ex2738.java
+++ algorithm_java/src/main/java/com/ex/baekjoon/배열2차원/ex2738.java
@@ -1,14 +1,32 @@
 package com.ex.baekjoon.배열2차원;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class ex2738 {
     public static void main(String[] args) {
-        // N*M크기의 두 행렬 A와 B가 주어졌을 때, 두 행렬을 더하는 프로그램을
-        BufferedReader br = new BufferedReader( new InputStreamReader(System.in));
-        StringTokenizer st =
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] arr1 = new int[n][m];
+        int[][] arr2 = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                arr1[i][j] = sc.nextInt();
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                arr2[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(arr1[i][j]+arr2[i][j]+" ");
+            }
+            System.out.println();
+        }
 
     }
 }
