c52d4f1661530fab0e2925f8077c5dc49e57544e
dominod <76812646+dbdomino@users.noreply.github.com>
2024-02-02 23:18:43 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 272 ms, Memory: 18716 KB -BaekjoonHub

---
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr0 = {25, 10, 5, 1};
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i<n; i++){
+            int n1 = sc.nextInt();
+            for (int j = 0; j< 4; j++) {
+                if (n1 / arr0[j] >0) sb.append(n1 / arr0[j] +" ");
+                else sb.append("0 ");
+                n1 = n1 % arr0[j];
+
+            }
+            sb.append("\n");
+        }
+        System.out.print(sb);
+    }
+}
