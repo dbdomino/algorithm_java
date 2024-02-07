fd7576daef61881233168d6213ef2bd841dbc3c4
dominod <76812646+dbdomino@users.noreply.github.com>
2024-02-07 16:38:03 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 144 ms, Memory: 14692 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
@@ -1,56 +1,55 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        //입력 보드
-        String[] arr = new String[N];
-        //흰보드와 검은보드
-        String[] Wboard = new String[8];
-        String[] Bboard = new String[8];
 
-        //입력보드에 입력값 저장
-        for(int i = 0; i < N; i++) {
-            arr[i] = br.readLine();
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        String[] arrs= new String[a];
+        for (int i = 0; i<a; i++) {
+            arrs[i] = br.readLine();
         }
 
-        //흰보드 만들기
-        for(int i = 0; i < 8; i++) {
-            if(i % 2 == 0) Wboard[i] = "WBWBWBWB";
-            else Wboard[i] = "BWBWBWBW";
+        String[] BlackArr = new String[8];
+        for (int i = 0; i<8; i++) {
+            if(i % 2 == 0) BlackArr[i] = "BWBWBWBW";
+            else BlackArr[i] = "WBWBWBWB";
+        }
+        String[] WhiteArr = new String[8];
+        for (int i = 0; i<8; i++) {
+            if(i % 2 == 0) WhiteArr[i] = "WBWBWBWB";
+            else WhiteArr[i] = "BWBWBWBW";
         }
 
-        //검은보드 만들기
-        for(int i = 0; i < 8; i++) {
-            if(i % 2 == 0) Bboard[i] = "BWBWBWBW";
-            else Bboard[i] = "WBWBWBWB";
-        }
+        int answer= 64; // 최대로 칠해야한다면 64개가 될것이다.
 
-        //모든 경우의 수중 제일 작은 값을 저장할 변수. 최솟값을 비교하기 위해서 나올 수 있는 제일 큰 값을 저장.
-        //8x8이기때문에 64가 제일 나올 수 있는 큰 수 이다.
-        //(완전 다른 두 보드를 비교하기 때문에 사실상 유효한 수는 절반인 32이다.)
-        int min = 64;
-
-        //보드르 비교하기 위한 for문 4개.
-        for(int i = 0; i < (N - 7); i++) { // 어디서 자를 지 위치에 따라서 경우의 수로 모두 비교하기 위해 
-            for(int j = 0; j < (M - 7); j++) {
-                int Wcount = 0;
+        for (int i = 0; i < (a-7); i++){
+            for(int j = 0; j < (b-7); j++) {
                 int Bcount = 0;
-                for(int k = 0; k < 8; k++) { // 비교하면서 흰보드일지 검은보드일지 에 따라 칠해야할 수를 구하고, 그중 최소를 뱉는다.
-                    for(int l = 0; l < 8; l++) {
-                        if(Wboard[k].charAt(l) != arr[k + i].charAt(l + j)) Wcount++;
-                        if(Bboard[k].charAt(l) != arr[k + i].charAt(l + j)) Bcount++;
+                int Wcount = 0;
+
+                for (int k = 0; k<8; k++){
+                    for (int l = 0; l<8; l++){ // 블랙먼저시작, 화이트먼저시작 중에 머가 더 칠해야되는 개수가 적은지 비교하기위해
+                        if (BlackArr[k].charAt(l) != arrs[i+k].charAt(j+l) ) Bcount++;
+                        if (WhiteArr[k].charAt(l) != arrs[i+k].charAt(j+l) ) Wcount++;
                     }
-                }
-                min = Math.min(min, Math.min(Wcount, Bcount)); // 지금껏 구한 최소들과 비교해서 가장 최소인 것을 저장한 뒤 나중에 출력
+                } // 그중에 최소 칠해야되는수를 찾아 answer에 저장한다.
+                answer = Math.min(answer, Math.min(Wcount, Bcount));
+//                System.out.println("i= "+i+" , j= "+j + " , answer = "+answer);
             }
         }
-        System.out.println(min);
+
+        System.out.println(answer);
+
+        br.close();
+
     }
 }
