a6c9f27ec03fe6f45d8312a231fa7ae6b3bedbb8
dominod <76812646+dbdomino@users.noreply.github.com>
2024-02-14 16:23:42 +0900

    [Silver IV] Title: 제로, Time: 240 ms, Memory: 23456 KB -BaekjoonHub

---
+++ 백준/Silver IV/10773. 제로/제로.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        //  항상 정신없는 재현이는 돈을 실수로 잘못 부르는 사고를 치기 일쑤였다.
+        //  잘못된 수를 부를 때마다 0을 외쳐서, 가장 최근에 재민이가 쓴 수를 지우게 시킨다.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        Stack<Integer> stack = new Stack<>();
+        for (int i=0; i<n; i++ ){
+            int a = Integer.parseInt(br.readLine());
+            if (a ==0) stack.pop();
+            else stack.push(a);
+        }
+
+        int sum = 0;
+        for(int o : stack) {	// 스택에 담긴 원소 합 구하기 
+            sum += o;
+        }
+
+        System.out.println(sum);
+
+        br.close();
+    }
+}
