7d64acb2cba7ee550fe905978e9cfb0b5c85262d
dominod <76812646+dbdomino@users.noreply.github.com>
2024-02-16 17:06:43 +0900

    [Silver III] Title: 도키도키 간식드리미, Time: 144 ms, Memory: 14428 KB -BaekjoonHub

---
+++ 백준/Silver III/12789. 도키도키 간식드리미/도키도키 간식드리미.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int[] line = new int[n]; //승환이 앞에 서있는 학생들의 수만큼 순서 저장하는 배열
+        for(int i = 0; i < n; i++) {
+            line[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Stack<Integer> stack = new Stack<>();
+
+        int cnt =1;
+        for (int i=0; i<n; i++){
+            int a = line[i];
+            if (a == cnt) cnt++;
+            else {
+                    //새로운 줄이이 비어있지 않을 때 가장 최근에 들어간 요소가 찾는 번호일 경우
+                if(!stack.isEmpty() && stack.peek() == cnt){
+                    stack.pop();
+                    i--; //임시 저장소에 있었지 stack이라는 공간에 있었던 것은 아니므로 한번더 반복해야한다.
+                    cnt++;
+                }else{ //찾는 번호가 아닐 경우 임시저장소에 넣는다
+                    stack.push(a);
+                }
+            }
+        }
+
+        int stacksize = stack.size();
+        boolean flag = true;
+        for (int i=0; i<stacksize; i++ ) {
+            int a = stack.pop();
+
+            if (a == cnt) cnt++;
+            else {flag=false; break;}
+        }
+        String s;
+        if (flag) s="Nice";
+        else s = "Sad";
+
+        System.out.println(s);
+
+    }
+}
